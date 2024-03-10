d1620607c7ed5f1462204ccddf38d4ab44a00112
rizki462 <73434166+rizki462@users.noreply.github.com>
2024-03-10 15:11:21 +0700

    Add files via upload
    Tugas dasar pemrograman

---
+++ Pertemuan 1.py
@@ -0,0 +1,6 @@
+nama = ("Rizki Kurnia")
+kelas = ("TI23M")
+jurusan = ("Teknik Informatika")
+quote = ("Semangat, semangat, semangat")
+
+print("nama = " + nama, "Kelas = " + kelas, "Jurusan = " + jurusan, "Catatan = " + quote)
